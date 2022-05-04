fdfc2799140241a03034bcabd7d43935bffb5ca8
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-05-05 01:36:31 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./.main.route");
 const connectToDB = require("./DBConnection/mongoDB");
-const PORT = 9009;
+const PORT = process.env.PORT || 9008;
 
 http.createServer(app).listen(PORT, () => {
   new connectToDB();
