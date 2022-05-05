4e8ee009d8e5a771d168129b067feb423a365130
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-05-05 16:05:07 +0530

    added params for get alldata

--- Models/blog.model.js
+++ Models/blog.model.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const BlogSchemas = new mongoose.Schema({
-    title:{type:String, required: true},
-    story:{type:String, required:true},
-    // author:{type:mongoose.Schema.Types.ObjectId,
-    userEmail:{type:String,required:true}
-    // ref:"user"}
-})
+  title: { type: String, required: true },
+  story: { type: String, required: true },
+  // author:{type:mongoose.Schema.Types.ObjectId,
+  fileUpload: { type: String },
+  userEmail: { type: String, required: true },
+  // ref:"user"}
+});
 
-module.exports= mongoose.model("BlogSchemas",BlogSchemas)
+module.exports = mongoose.model("BlogSchemas", BlogSchemas);
